daa42bedd9b449449761f9b36b8e6b72bb1d533c
41223137 <144768414+41223137@users.noreply.github.com>
2024-01-14 02:01:44 +0000

    新增ANSIC 1～5頁面

---
+++ downloads/ansic1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()  
+{
+    // Print Name
+    printf("Name   : 黃子懿\n"); 
+
+    // Print Date of Birth
+    printf("DOB    : December 6, 2004\n"); 
+
+    // Print Mobile Number
+    printf("Mobile : null\n"); 
+
+    // Indicate successful execution
+    return(0); 
+}
